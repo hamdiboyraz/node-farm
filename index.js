8875bfe1eed7f153c8738796bab687c21a3e2878
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-12-15 15:35:31 +0300

    Routing

--- index.js
+++ index.js
@@ -39,8 +39,32 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
 
 //////////////////////////////////
 // SERVER
-const server = http.createServer((req, res) => {
+/* const server = http.createServer((req, res) => {
     res.end('Hello from the server!')
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to request on port 8000');
+}) */
+
+////////////////////////////////////
+// ROUTING
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else  {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-type': 'hello-world'
+        });
+        res.end('<h1>Page Not Found</h1>')
+    }    
 });
 
 server.listen(8000, '127.0.0.1', () => {
