7ccdbea83e5ebfcf61228c92f1f22605ad28856e
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-12-15 14:26:57 +0300

    Read and write files with callback functions/async way

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// Blocking, synchronous way
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
 
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', 'utf-8');
-console.log('File written!');
+fs.writeFileSync("./txt/output.txt", "utf-8");
+console.log("File written!");
+
+// Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log("2", data);
+});
+console.log("1", "Will read file!");
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR! 💥");
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has been written 😁");
+      });
+    });
+  });
+});
+
+/*  //instead of arrow function(ES6)
+    function(err, data) {
+
+    } 
+*/
