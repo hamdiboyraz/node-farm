ae8fb5887d57aa26117b7528f6d905d26c89a2c9
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-12-15 21:40:12 +0300

    Build and fill templates

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ server.listen(8000, '127.0.0.1', () => {
 ///////////////////////
 // SIMPLE WEB API
 
-// This code is top-level, so we can execute sync, just executed once
+/* // This code is top-level, so we can execute sync, just executed once
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 
 // Convert to JS object
@@ -95,6 +95,76 @@ const server = http.createServer((req, res) => {
       "Content-type": "application/json",
     });
     res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page Not Found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+}); */
+
+///////////////////////////////////
+// HTML TEMPLATING
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, "utf-8");
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  // Overview page
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    // return array so we use join method via ""
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    // Product page
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+
+    // API
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+
+    // Not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
